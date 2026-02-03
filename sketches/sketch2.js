c34ca2c5d4e2bba684e3e4a17ae6baff7397f263
emi <kosakura.emi@gmail.com>
2026-02-03 14:17:59 -0800

    display current time

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -1,5 +1,6 @@
 // Instance-mode sketch for tab 2
 registerSketch('sk2', function (p) {
+
   p.setup = function () {
     p.createCanvas(800, 800);
   };
@@ -8,7 +9,13 @@ registerSketch('sk2', function (p) {
     p.fill(100, 150, 240);
     p.textSize(32);
     p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. A', p.width / 2, p.height / 2);
+    let h = p.hour();
+    let m = p.minute();
+    let s = p.second();
+
+    let timeStr = `${p.nf(h, 2)}:${p.nf(m, 2)}:${p.nf(s, 2)}`;
+
+    p.text(timeStr, p.width / 2, p.height / 2);
   };
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
 });
