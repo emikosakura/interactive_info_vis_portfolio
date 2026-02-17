85a49acb083cb766b0d0903acb10f56254acc05e
emi <kosakura.emi@gmail.com>
2026-02-16 17:04:41 -0800

    create variables and initialize count

--- sketches/sketch5.js
+++ sketches/sketch5.js
@@ -1,6 +1,17 @@
 registerSketch('sk5', function (p) {
   let table;
 
+  const months = [
+    "January","February","March","April","May","June",
+    "July","August","September","October","November","December"
+  ];
+
+  let monthCounts = {};
+  let monthTypeCounts = {};
+  let maxMonthTotal = 0;
+  let totalAllDrinks = 0;
+  let hoveredMonth = null;
+
   p.preload = function () {
     table = p.loadTable("boba-tracker.csv", "csv", "header");
   };
@@ -12,6 +23,12 @@ registerSketch('sk5', function (p) {
   p.draw = function () {
     p.background(255);
     p.textFont('monospace');
+    p.angleMode(p.RADIANS);
+
+    for (const m of months) {
+      monthCounts[m] = 0;
+      monthTypeCounts[m] = { Milk: 0, Fruit: 0 };
+    }
   }
 
   p.windowResized = function () { p.resizeCanvas(800, 800); };
